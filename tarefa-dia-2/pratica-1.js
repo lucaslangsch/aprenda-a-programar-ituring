904aefc578821d9612a6c26085640256c2ad1efe
lucaslangsch <lucas.langsch@gmail.com>
2022-08-24 18:17:15 -0300

    subindo atividade 2 restante

---
+++ tarefa-dia-2/pratica-1.js
@@ -0,0 +1,7 @@
+//Declaração de variáveis
+const nome = "Lucas"
+const email = "lucas.langsch@gmail.com"
+let profissao = "estudante"
+let idade = 28
+let sonho_conhercer = "Tailândia"
+console.log('Meu nome é ' + nome + '\nMeu email é ' + email + '\nMinha profissão atualmente é ' + profissao + '\nEu tenho hoje ' + idade + ' anos \nE meu senho é conhecer a ' + sonho_conhercer)
