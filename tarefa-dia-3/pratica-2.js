beb8b3a7467110407cc6dc4e26c1fbbecedfdb6d
lucaslangsch <lucas.langsch@gmail.com>
2022-08-27 16:47:43 -0300

    subindo prática 2

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+//Crie um código que calcule e mostre a tabuada de 1 a 10 de um fator n escolhido pelo usuário
+
+let i = 1
+let n = 8
+
+while (i < 11) {
+    let multiplicacao = i * n
+    console.log(`${n} x ${i} = ${multiplicacao}`)
+    i++
+}
